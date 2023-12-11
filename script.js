9951ece4c1081e0ae6c0a0e7419e9f702ea57d0e
Nitin Gumber <aroranitin486@gmail.com>
2023-12-11 23:08:32 +0530

    DOM Basic to Advance

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ titlestyle.innerHTML
 // querySelector VS querySelectorAll
 
 const myUl = document.querySelector('ul')
-console.log(myUl)
+// console.log(myUl)
 const turngreen = myUl.querySelector('li')
 turngreen.style.color = "green"
 turngreen.style.padding = "1rem 1rem"
@@ -26,9 +26,43 @@ tempLiList.forEach((items) => items.style.color = "green")
 // HTMLCollection
 
 const tempClassList = document.getElementsByClassName("list-item")
-console.log(tempClassList)
+// console.log(tempClassList)
 const convertToArray = Array.from(tempClassList)
-console.log(convertToArray)
+// console.log(convertToArray)
 convertToArray.map((item) =>{
     item.style.color = "orange"
 })
+
+// Parent ko select krke child element ko display krna
+const parent = document.querySelector('.parent')
+// console.log(parent)
+// console.log(parent.children)
+// console.log(parent.children[1].innerHTML)
+for (let i = 0; i < parent.children.length; i++) {
+    // console.log(parent.children[i].innerHTML)
+}
+
+parent.children[1].style.color = "red"
+parent.children[2].style.padding = "1rem 1rem"
+// console.log(parent.firstElementChild)
+// console.log(parent.lastElementChild)
+
+// Child element ko select krke parent element ko select kr skte hai
+const dayOne = document.querySelector('.day')
+// console.log(dayOne)
+// console.log(dayOne.parentElement)
+// console.log(dayOne.nextElementSibling)
+// console.log("NODES: ", parent.childNodes) // line break count krta h
+
+// How to Create Element:- 
+const div = document.createElement('div')
+// console.log(div)
+div.className = "main"
+div.id = Math.round(Math.random() * 10 + 1)
+div.setAttribute("title", "generated title")
+div.style.background = "green"
+// div.innerHTML = "Chai aur Code"
+const addText = document.createTextNode("Chai aur Code")
+div.appendChild(addText)
+document.body.appendChild(div)
+
